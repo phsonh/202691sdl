241141de582e5dfc633a482a619b91eca0cdb904
Lonely Owl <benhuangxianan@gmail.com>
2026-09-03 16:19:59 +0800

    帧计时器

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,11 @@ int main(int argc, char* argv[])
     core::modules::Window::Engine_Window* window = core::modules::Window::Create(1280, 960);
     //初始化帧
     core::runtime::Frame::Init();
+    //主循环
     while (core::runtime::Frame::IsRunning())
     {
+        core::runtime::Frame::Begin();
+
         // 处理输入
         SDL_Event event;
         while (SDL_PollEvent(&event))
@@ -28,6 +31,8 @@ int main(int argc, char* argv[])
             }
         }
         core::modules::Window::SetTitle(window, std::to_string(FrameCount).c_str());
+
+        core::runtime::Frame::End();
     }
 
     core::modules::Window::Destroy(window);
