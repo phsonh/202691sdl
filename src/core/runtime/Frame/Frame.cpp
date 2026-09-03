d5a65cca05e6997e09b64a30f5e2ff25e167ddf7
Lonely Owl <benhuangxianan@gmail.com>
2026-09-03 16:42:19 +0800

    帧计时器

--- src/core/runtime/Frame/Frame.cpp
+++ src/core/runtime/Frame/Frame.cpp
@@ -4,23 +4,35 @@
 
 namespace core::runtime::Frame {
 	namespace {
+		constexpr std::uint64_t NS_PER_SECOND = 1'000'000'000;
 		std::uint64_t FrameCount;
-		std::uint64_t FrameStartTime;
+		std::uint64_t ThisFrameStartTime;
+		std::uint64_t LastFrameStartTime;
+		std::uint64_t LastFrameUsedTime;
+		std::uint64_t FrameBudgetTime;
 		int TargetFPS;
 		double CurrentFPS;
 		bool running;
 	}
 	void Init() {
 		FrameCount = 0;
-		FrameStartTime = 0;
+		ThisFrameStartTime = 0;
+		LastFrameUsedTime = 0;
 		TargetFPS = 60;
 		running = true;
+		FrameBudgetTime = NS_PER_SECOND / TargetFPS;
 	}
 	void Begin() {
-		FrameStartTime = SDL_GetTicksNS();
+		LastFrameStartTime = ThisFrameStartTime;
+		ThisFrameStartTime = SDL_GetTicksNS();
+		LastFrameUsedTime = ThisFrameStartTime - LastFrameStartTime;
 		FrameCount++;
 	}
 	void End() {
+
+		if ((SDL_GetTicksNS() - ThisFrameStartTime) < FrameBudgetTime) {
+			SDL_DelayPrecise(FrameBudgetTime - (SDL_GetTicksNS() - ThisFrameStartTime));
+		}
 	}
 	std::uint64_t GetFrameCount() {
 		return FrameCount;
